d61b52d7db487229b64ac07461bbbcb39b78cce9
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-10-22 03:24:42 +0500

    Task5

---
+++ Tyuiu.ZakharovaVI.Sprint3.Task5.V28.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ZakharovaVI.Sprint3.Task5.V28.Lib
+{
+    public class DataService : ISprint3Task5V28
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, k;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries += Math.Pow(k, x) / Math.Sin(k);
+                }
+            }
+            return Math.Round(sumSeries, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaVI.Sprint3.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.ZakharovaVI.Sprint3.Task5.V28.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint3.Task5.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 3;
+            int stopValue1 = 1;
+            int stopValue2 = 12;
+
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            double wait = -349.648;
+            Assert.AreEqual(wait, res);
+
+
+
+        }
+    }
+}
