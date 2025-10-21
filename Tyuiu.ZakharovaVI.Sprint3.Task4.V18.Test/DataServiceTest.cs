8e41211c469b53b2500695c1ef711f00efb98cb6
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-10-22 02:55:13 +0500

    Task4

---
+++ Tyuiu.ZakharovaVI.Sprint3.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.ZakharovaVI.Sprint3.Task4.V18.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint3.Task4.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 2.993;
+
+            Assert.AreEqual(wait, res); 
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaVI.Sprint3.Task4.V18/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.ZakharovaVI.Sprint3.Task4.V18.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint3.Task4.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Захарова В. И. | ИБКС-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Захарова Валерия Игоревна | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* вычислить значение функции y=sin(x)/cos(x)                              *");
+            Console.WriteLine("* При х = 0 прервать цикл.                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadLine();
+        }
+    }
+}
