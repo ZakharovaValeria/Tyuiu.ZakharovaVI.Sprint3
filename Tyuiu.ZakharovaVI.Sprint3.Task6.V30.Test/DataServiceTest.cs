4a5735207e48f6d2866202dfc325d5d598ca1699
ZakharovaValeria <Valeria122610Z@yandex.ru>
2025-10-23 15:27:38 +0500

    Task6

---
+++ Tyuiu.ZakharovaVI.Sprint3.Task6.V30.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.ZakharovaVI.Sprint3.Task6.V30.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint3.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 25;
+
+            Assert.AreEqual(wait, res);
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.ZakharovaVI.Sprint3.Task6.V30/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ZakharovaVI.Sprint3.Task6.V30.Lib;
+
+namespace Tyuiu.ZakharovaVI.Sprint3.Task6.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Захарова В. И. | ИБКС-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнила: Захарова Валерия Игоревна | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [11, 17] количество всех делителей.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadLine();
+        }
+    }
+}
